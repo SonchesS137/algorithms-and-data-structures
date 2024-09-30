c347b8f10c5492df13a7b21eb54f65b7ef357afd
SonchesS137 <saserova@edu.hse.ru>
2024-09-30 16:50:10 +0300

    Lab0

--- lab0/task1/src/subtask_1/solution.py
+++
@@ -1,6 +0,0 @@
-s = input().split()
-a = int(s[0])
-b = int(s[1])
-if (a or b) <= 10**9 and (a or b) >= -10**9:
-    print(a + b)
-    
--- lab0/task1/src/subtask_3/solution.py
+++
@@ -1,8 +0,0 @@
-with open("input.txt", 'r') as file:
-    data = file.read().replace('\n', '')
-a = data.split()
-num1 = int(a[0])
-num2 = int(a[1])
-result = num1 + num2
-with open("output.txt", 'w') as file:
-    file.write(str(result))
